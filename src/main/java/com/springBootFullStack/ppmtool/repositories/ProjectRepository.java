288943779e3414cab93ba5f63898088f10a32a8a
Bruce-Decker <bruce.d008@gmail.com>
2022-01-17 20:39:42 -0800

    project service and repository

---
+++ src/main/java/com/springBootFullStack/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.springBootFullStack.ppmtool.repositories;
+
+import com.springBootFullStack.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+     @Override
+     Iterable<Project> findAllById(Iterable<Long> iterable);
+}
